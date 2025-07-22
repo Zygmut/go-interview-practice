0cbf929fa17fb576b3bd862da25a3dd3ede603b6
Zygmut <deltas.game@gmail.com>
2025-07-22 21:19:53 +0200

    Challenge 2

---
+++ challenge-2/submissions/Zygmut/solution-template.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Read input from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		input := scanner.Text()
+
+		// Call the ReverseString function
+		output := ReverseString(input)
+
+		// Print the result
+		fmt.Println(output)
+	}
+}
+
+// ReverseString returns the reversed string of s.
+func ReverseString(s string) string {
+	sol := ""
+
+	for idx := len(s) - 1; idx >= 0; idx-- {
+		sol += string(s[idx])
+	}
+
+	return sol
+}
